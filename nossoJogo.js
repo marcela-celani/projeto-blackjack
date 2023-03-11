1ea212d806c7622a14200ef4a8cd1d7ff9602c17
marcela-celani <marcelacelani.ti@gmail.com>
2023-03-11 20:56:10 -0300

    Projeto BlackJack fase 'nosso jogo' finalizada

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,28 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.log('Boas vinhas ao jogo de BlackJack!')
+
+if(confirm('Quer iniciar uma nova rodada?')){
+   // Tirando as duas cartas do usuário
+   const carta1 = comprarCarta()
+   const carta2 = comprarCarta()
+   const somaUsuario = carta1.valor + carta2.valor
+      
+   console.log(`Usuário - cartas: ${carta1.texto} ${carta2.texto} - pontuação = ${somaUsuario}`)
+
+   // Tirando as duas cartas do computador
+   const carta3 = comprarCarta()
+   const carta4 = comprarCarta()
+   const somaComputador = carta3.valor + carta4.valor
+      
+   console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação = ${somaComputador}`)
+
+   if(somaUsuario > somaComputador) {
+      console.log('O usuário ganhou!')
+   } else if (somaComputador > somaUsuario) {
+      console.log('O computador ganhou!')
+   } else {
+      console.log('Empate!')
+   }
+
+} else {
+   console.log('O jogo acabou.')
+}
